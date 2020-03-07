46bb99d105947726b37159a8765376d5ab1e2df9
Justin Budlow <justin.budlow@gmail.com>
2020-03-06 19:07:19 -0500

    presidents homework

--- pset-iterators.js
+++ pset-iterators.js
@@ -43,6 +43,15 @@ console.log('Problem 1:')
 
 // Add your code below this line
 
+const firstWhigPresident = presidents.find(function (president) {
+  /*
+    Using .find() because we are only interested in returning the first element that meets the condition
+  */
+  return president.party === 'Whig'
+})
+
+console.log(firstWhigPresident.president)
+
 // Add your code above this line
 
 /** added for formatting purposes **/
@@ -65,6 +74,15 @@ Expected Result Shape: An array of objects (with each object representing a pres
 console.log('Problem 2:')
 
 // Add your code below this line
+
+const presidentsNamedJames = presidents.filter((president) => {
+  // console.log(president)
+  return president.president.includes("James")
+
+})
+
+console.log(presidentsNamedJames)
+
 
 // Add your code above this line
 
@@ -93,6 +111,14 @@ console.log('Problem 3:')
 
 // Add your code below this line
 
+const presidentialParties = presidents.map((president) => {
+  // console.log(president)
+  return president.party
+})
+
+console.log(presidentialParties)
+
+
 // Add your code above this line
 
 /** added for formatting purposes **/
@@ -114,6 +140,15 @@ Expected Result Shape: An array of objects (with each object representing a pres
 console.log('Problem 4:')
 
 // Add your code below this line
+
+const presidentsBetween1850and1900 = presidents.filter((president) => {
+
+  let yearTookOffice = president.took_office.slice(0,4)
+
+  return yearTookOffice > 1850 && yearTookOffice < 1900
+})
+
+console.log(presidentsBetween1850and1900)
 
 // Add your code above this line
 
@@ -138,6 +173,13 @@ console.log('Problem 5:')
 
 // Add your code below this line
 
+const livingPresidents = presidents.filter((president) => {
+
+  return president.death_year === null
+})
+
+console.log(livingPresidents)
+
 // Add your code above this line
 
 /** added for formatting purposes **/
@@ -161,6 +203,14 @@ console.log('Problem 6:')
 
 // Add your code below this line
 
+const firstRepublican = presidents.find(function (president) {
+
+  return president.party === 'Republican'
+})
+
+console.log(firstRepublican.president)
+
+
 // Add your code above this line
 
 /** added for formatting purposes **/
@@ -183,6 +233,14 @@ Expected Result Shape: An array of objects (with each object representing a pres
 console.log('Problem 7:')
 
 // Add your code below this line
+
+const shortTermPresidents = presidents.filter((president) => {
+
+  return (parseInt(president.left_office) - parseInt(president.took_office)) < 4
+
+})
+
+console.log(shortTermPresidents)
 
 // Add your code above this line
 
@@ -211,6 +269,27 @@ console.log('Problem 8:')
 
 // Add your code below this line
 
+const firstNameCount = presidents.reduce((nameTally, current)=>{
+
+// the "president" attribute in the presidents array is a full name, so split out each full name into an array of names (ex: ['first', 'last'])
+const presidentNameArray = current.president.split(' ')
+
+//the first name is always the first element of the presidentNameArray array. Store the first name is the firstNames variable
+const firstName = presidentNameArray[0]
+
+//each time through, the function looks at the first name based on how it's stored in the firstNames variable, and adds or increments to the firstNameCount object as necessary
+  if (nameTally[firstName] === undefined) {
+    // if a first name does not already exist in the object
+    nameTally[firstName] = 1
+  } else {
+    // first name already exists in letterCount object
+    // increment value by 1
+    nameTally[firstName] = nameTally[firstName] + 1
+  }
+  return nameTally
+}, {})
+
+console.log(firstNameCount)
 
 
 // Add your code above this line
